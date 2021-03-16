d0fe5ab69d69f1771cdf5778ed14aa8c21d5baaf
mrojase <mrojase@snapfinance.com>
2021-03-15 19:28:06 -0600

    refactoring App.js and Search.js with hooks instead of class components

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,57 @@
-import React from 'react';
-import Searchbar from './Searchbar';
-import youtube from '../apis/Youtube';
-import VideoList from '../components/VideoList';
-import VideoDetail from './VideoDetail';
+import React, { useState, useEffect } from "react";
+import Searchbar from "./Searchbar";
+import youtube from "../apis/Youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
-const API_KEY = 'AIzaSyAHhp6pn72yNgq_Xw6bqlgUFyu6zWi3fNQ';
+const API_KEY = "AIzaSyAHhp6pn72yNgq_Xw6bqlgUFyu6zWi3fNQ";
 
-class App extends React.Component {
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-    state = { videos: [], selectedVideo: null };
+  useEffect(() => {
+    videoSearch("React JS");
+  },[]);
 
-    videoSearch = async (term) => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: term,
-                key: API_KEY,
-                part: 'snippet',
-                type: 'video',
-                maxResults: 5,
-            }
-        });
+  const videoSearch = async (term) => {
+    const {
+      data: { items },
+    } = await youtube.get("/search", {
+      params: {
+        q: term,
+        key: API_KEY,
+        part: "snippet",
+        type: "video",
+        maxResults: 5,
+      },
+    });
 
-        this.setState({
-            videos: response.data.items,
-            selectedVideo: response.data.items[0]
-        })
-    }
+    setVideos(items);
+    setSelectedVideo(items[0]);
+  };
 
-    onVideoSelect = (video) => {
-        this.setState({ selectedVideo: video })
-    }
+  const onVideoSelect = (video) => {
+    setSelectedVideo(video);
+  };
 
-    render() {
-        return (
-            <div className="ui container">
-                <Searchbar videoSearch={this.videoSearch} />
-                <div className="ui grid">
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            <VideoDetail video={this.state.selectedVideo} />
-                        </div>
-                        <div className="five wide column">
-                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className="ui container">
+        <Searchbar videoSearch={videoSearch} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={selectedVideo} />
             </div>
-        )
-    }
-}
+            <div className="five wide column">
+              <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
